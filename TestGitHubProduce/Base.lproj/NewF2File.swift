20d2b48dbe31d2f579621db6b2585f4882a23812
Tyler Boston <tboston6789@gmail.com>
2021-05-01 17:31:11 -0500

    the beginning of this featurebranch2

---
+++ TestGitHubProduce/Base.lproj/NewF2File.swift
@@ -0,0 +1,23 @@
+//
+//  NewF2File.swift
+//  TestGitHubProduce
+//
+//  Created by Tyler Boston on 5/1/21.
+//
+
+import UIKit
+
+class NewF2File: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    func printNewStuff () {
+        print("This is a new start F2")
+    }
+    
+}
--- TestGitHubProduce/ViewController.swift
+++ TestGitHubProduce/ViewController.swift
@@ -12,12 +12,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("Hello World, this in the mainline")
+        func printFromF1() {
+        print("Hello World, this is code from a featurebranch")
         print("Modifications by Tylerx1")
         print("Modification by who? w/o conflicting code")
         print("modification by Tylyr w/o conflicting code x 2, also a new addition to this line as referenced in line 19 ")
         print("last modification by Tyler w/ conflicting code on line 17 and 18")
     }
+        func printFromF2() {
+            
+            print("Hello world, this is code from F2 that will be merged into the mainline, and merged into featurebranch1 to determine what it's like to work side by side with continuous integrators")
+        }
+    }
+    
 
   
 }
